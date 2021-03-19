b1b1af2518d0571d67a515bbac8ecf3b7523d8cc
André Nascimento <andrevsn1@gmail.com>
2021-03-19 15:34:40 -0300

    change from js to tsx

---
+++ next-frontend/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>News template grid</title>
+      </Head>
+      <h1>Title</h1>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default Home
