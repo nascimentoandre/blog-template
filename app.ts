2c7281799956aa7fa4dd6f2b19fd855700d8c62e
André Nascimento <andrevsn1@gmail.com>
2021-03-22 21:45:58 -0300

    include some types, new test route

--- app.ts
+++ app.ts
@@ -1,26 +1,29 @@
 import express from "express";
+import { Request, Response, NextFunction } from "express";
 import createError from "http-errors";
 import logger from "morgan";
 
 import indexRouter from "./routes/index";
+import postsRouter from "./routes/posts";
 
 let app = express();
 
 // middleware
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // adding routes
 app.use("/", indexRouter);
+app.use("/posts", postsRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   next(createError(404));
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
