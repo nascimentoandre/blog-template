573e1f0be34dd99f4c35f7ea1880065315375200
André Nascimento <andrevsn1@gmail.com>
2021-03-22 21:48:42 -0300

    test posts route

--- next-frontend/pages/index.tsx
+++ next-frontend/pages/index.tsx
@@ -1,13 +1,15 @@
 import Head from 'next/head'
+import PlotPost from "../components/posts"
 
 const Home: React.FC = () => {
   return (
     <div>
       <Head>
-        <title>News template grid</title>
+        <title>Blog</title>
       </Head>
       <h1>Title</h1>
       <p>Hello world</p>
+      <PlotPost />
     </div>
   )
 }
