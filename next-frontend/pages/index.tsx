d30fb9fe78db16a9f29b21b4cc4605ebba781a8b
André Nascimento <andrevsn1@gmail.com>
2021-03-23 19:17:37 -0300

    fetching data, but something feels off

--- next-frontend/pages/index.tsx
+++ next-frontend/pages/index.tsx
@@ -1,7 +1,18 @@
 import Head from 'next/head'
-import PlotPost from "../components/posts"
+import { GetServerSideProps } from 'next'
 
-const Home: React.FC = () => {
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch("http://localhost:3001/posts")
+  const posts = await res.json()
+
+  return {
+    props: {
+      posts,
+    }
+  }
+}
+
+function Home({ posts }) {
   return (
     <div>
       <Head>
@@ -9,7 +20,7 @@ const Home: React.FC = () => {
       </Head>
       <h1>Title</h1>
       <p>Hello world</p>
-      <PlotPost />
+      <p>{posts.text}</p>
     </div>
   )
 }
