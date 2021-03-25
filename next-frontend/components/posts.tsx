519f8f23eb5c17f1cb6c69d4496f6fafb550975a
André Nascimento <andrevsn1@gmail.com>
2021-03-25 20:25:54 -0300

    actually display post comp

--- next-frontend/components/posts.tsx
+++ next-frontend/components/posts.tsx
@@ -1,27 +1,12 @@
-import React, { useState, useEffect } from "react"
-//import { InferGetServerSidePropsType } from 'next'
-import { GetServerSideProps } from 'next'
+import { PostStyle } from "../styles/pages/home"
+import Image from "next/image"
 
-interface Kanye {
-  quote: string
-}
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const req = await fetch("https://api.kanye.rest/")
-  const posts: Kanye = await req.json()
-
-  return {
-    props: {posts}
-  }
-}
-
-function PlotPost({ posts }) {
-  console.log(posts)
-  return (
-    <div>
-      <p>Hold on</p>
-    </div>
+const Post: React.FC = () => {
+  return(
+    <PostStyle>
+      <Image src="https://picsum.photos/400/200" width={400} height={200}/>
+    </PostStyle>
   )
 }
 
-export default PlotPost
+export default Post
