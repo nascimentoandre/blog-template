decc6ea2d4b2905a7345e0131ed047fbb1470df7
André Nascimento <andrevsn1@gmail.com>
2021-03-25 20:27:48 -0300

    update landing page

--- next-frontend/pages/index.tsx
+++ next-frontend/pages/index.tsx
@@ -1,26 +1,42 @@
 import Head from 'next/head'
 import { GetServerSideProps } from 'next'
+import api from '../services/api'
+import Post from '../components/posts'
+import Navbar from '../components/navbar'
+import { HomeContainer, PostFlexbox } from '../styles/pages/home'
+import Footer from '../components/footer'
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await fetch("http://localhost:3001/posts")
-  const posts = await res.json()
+//export const getServerSideProps: GetServerSideProps = async () => {
+  ////const res = await fetch("http://localhost:3001/posts")
+  //const res = await api.get("/posts")
+  //const posts = res.data
 
-  return {
-    props: {
-      posts,
-    }
-  }
-}
+  //return {
+    //props: {
+      //posts,
+    //}
+  //}
+//}
 
-function Home({ posts }) {
+function Home() {
   return (
     <div>
       <Head>
         <title>Blog</title>
       </Head>
-      <h1>Title</h1>
-      <p>Hello world</p>
-      <p>{posts.text}</p>
+      <Navbar />
+      <HomeContainer>
+        <h1>Latest posts</h1>
+        <PostFlexbox>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </PostFlexbox>
+      </HomeContainer>
+      <Footer />
     </div>
   )
 }
