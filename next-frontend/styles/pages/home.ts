8da6f718b37bc20d9301421987d53d34f5e1455c
André Nascimento <andrevsn1@gmail.com>
2021-03-25 20:24:53 -0300

    main page style

---
+++ next-frontend/styles/pages/home.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+export const HomeContainer = styled.div`
+  margin: 2rem 5vw 5rem;
+`
+
+export const PostFlexbox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`
+export const PostStyle = styled.div`
+  border: 1px solid black;
+  width: 400px;
+  height: 400px;
+  border-radius: 7px;
+  margin-bottom: 4rem;
+`
