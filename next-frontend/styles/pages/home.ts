0816fc5564f96b92d6027bc2239414c1c8ddb564
André Nascimento <andrevsn1@gmail.com>
2021-03-26 17:08:42 -0300

    modify

--- next-frontend/styles/pages/home.ts
+++ next-frontend/styles/pages/home.ts
@@ -12,7 +12,28 @@ export const PostFlexbox = styled.div`
 export const PostStyle = styled.div`
   border: 1px solid black;
   width: 400px;
-  height: 400px;
+  height: 450px;
   border-radius: 7px;
   margin-bottom: 4rem;
+
+  a {
+    color: ${props => props.theme.colors.hover1};
+    text-decoration: none;
+  }
+
+  .info {
+    padding: 0rem 1rem 0rem;
+    margin-top: -1rem;
+  }
+
+  .tags {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+
+  .tags a {
+    color: black;
+  }
 `
