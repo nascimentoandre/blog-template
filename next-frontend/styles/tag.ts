7260d78c8f244390a58c72d0b9f2877e89aaaf6b
André Nascimento <andrevsn1@gmail.com>
2021-03-26 17:07:21 -0300

    create tag style

---
+++ next-frontend/styles/tag.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const TagStyle = styled.div`
+  height: 20px;
+  border: 1px solid black;
+  font-size: 14px;
+  border-radius: 15px;
+  width: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+`
