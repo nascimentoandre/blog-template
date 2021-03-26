291233fa986f5825a9a591a6e313f92c80e56b50
André Nascimento <andrevsn1@gmail.com>
2021-03-26 17:08:19 -0300

    finish post layout

--- next-frontend/components/posts.tsx
+++ next-frontend/components/posts.tsx
@@ -1,10 +1,22 @@
 import { PostStyle } from "../styles/pages/home"
 import Image from "next/image"
+import Link from "next/link"
+import Tag from "./tag"
 
 const Post: React.FC = () => {
   return(
     <PostStyle>
       <Image src="https://picsum.photos/400/200" width={400} height={200}/>
+      <div className="info">
+        <h1>Post title</h1>
+        <p>Maecenas nulla enim, varius sit amet efficitur a, mollis vitae quam. Vestibulum sit amet posuere Donec nisi velit, hendrerit pharetra mauris vel, egestas dictum nisl ... <Link href="/">Read more</Link></p>
+        <div className="tags">
+          <p>Tags: </p>
+          <Tag tag="Sports" bgColor="yellow" />
+          <Tag tag="Health" bgColor="green" />
+          <Tag tag="Fitness" bgColor="cyan" />
+        </div>
+      </div>
     </PostStyle>
   )
 }
