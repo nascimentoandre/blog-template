ac03acca0dfd449e6792b935f23993ac4ce4f091
André Nascimento <andrevsn1@gmail.com>
2021-04-12 13:15:07 -0300

    add description field

--- tables/post.sql
+++ tables/post.sql
@@ -1,6 +1,7 @@
 create table posts (
   id bigserial not null primary key,
-  title varchar(255) not null,
+  title varchar(100) not null,
+  description varchar(150),
   content text default '...',
   tags text[],
   created_at timestamp default now()
