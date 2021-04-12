bdf6731894d2eb8d286c1d1cc83a93b6ac766654
André Nascimento <andrevsn1@gmail.com>
2021-04-12 13:16:47 -0300

    tags with random colors

--- next-frontend/components/tag.tsx
+++ next-frontend/components/tag.tsx
@@ -3,12 +3,17 @@ import Link from "next/link"
 
 interface Props {
   tag: string,
-  bgColor: string,
 }
 
-const Tag: React.FC<Props> = ({tag, bgColor}) => {
+const randomColor: (() => string) = () => {
+  let colors = ["gold", "#7FFFD4", "magenta", "#FAEBD7", "#A522A2A", "#8a2be2", "#d2691e", "#dc143c", "#dbd76b", "#e9967a", "#8fbc8f", "#adff2f", "#ff69b4"]
+  return colors[Math.round(Math.random()*(colors.length-1))]
+}
+
+const Tag: React.FC<Props> = ({ tag }) => {
+  let color = randomColor();
   return (
-    <TagStyle style={{backgroundColor: bgColor}}>
+    <TagStyle style={{backgroundColor: color}}>
       <Link href="/">{tag}</Link>
     </TagStyle>
   )
