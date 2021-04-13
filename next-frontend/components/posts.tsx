9e5f4605e7e79087eda48ad3f7bd09150c606093
André Nascimento <andrevsn1@gmail.com>
2021-04-13 16:47:57 -0300

    minor date change

--- next-frontend/components/posts.tsx
+++ next-frontend/components/posts.tsx
@@ -12,31 +12,16 @@ interface Props {
   createdAt: Date
   }
 
-const Post: React.FC<Props> = ({ title, description, content, createdAt, tags }) => {
+const Post: React.FC<Props> = ({ id, title, description, content, createdAt, tags }) => {
   if (!description) description = content.slice(0, 150);
-  let d = new Date(createdAt);
-
-  console.log(tags);
+  let d = new Date(createdAt)
 
   return(
-//    <PostStyle>
-//      <Image src="https://picsum.photos/400/200" width={400} height={200}/>
-//      <div className="info">
-//        <h1>Post title</h1>
-//        <p>Maecenas nulla enim, varius sit amet efficitur a, mollis vitae quam. Vestibulum sit amet posuere Donec nisi velit, hendrerit pharetra mauris vel, egestas dictum nisl ... <Link href="/">Read more</Link></p>
-//        <div className="tags">
-//          <p>Tags: </p>
-//          <Tag tag="Sports" bgColor="yellow" />
-//          <Tag tag="Health" bgColor="green" />
-//          <Tag tag="Fitness" bgColor="cyan" />
-//        </div>
-//      </div>
-//    </PostStyle>
       <PostStyle>
         <div className="info">
-          <h1>{title}</h1>
-          <p className="date">Created at: {d.toLocaleDateString()}, {d.toLocaleTimeString()}</p>
-          <p className="description"> {description}...<Link href="/">Read more</Link> </p>
+          <h1><Link href={`/posts/${id}`}>{title}</Link></h1>
+          <p className="date">{d.toLocaleDateString()}, {d.toLocaleTimeString()}</p>
+          <p className="description"> {description}...<Link href={`/posts/${id}`}>Read more</Link> </p>
           <div className="tags">
             <p>Tags: </p>
             <div className="tag-container">
