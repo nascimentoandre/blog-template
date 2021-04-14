8c9a20ced8ac94b0209fe0b7792f1338d8c1d817
André Nascimento <andrevsn1@gmail.com>
2021-04-14 09:17:33 -0300

    finish create page layout

--- next-frontend/pages/create.tsx
+++ next-frontend/pages/create.tsx
@@ -1,25 +1,34 @@
 import Head from "next/head"
+import Link from "next/link"
 import { CreateContainer } from "../styles/pages/create"
 import Navbar from "../components/navbar"
 import Footer from "../components/footer"
+import Btn1 from "../components/confirmBtn"
 
 const Create: React.FC = () => {
   return (
     <>
     <Navbar />
     <CreateContainer>
-      <h1>Create a new post</h1>
-      <label>Write a title</label>
+      <h1>Create a new markdown post</h1>
+      <label>Write a title (max. 100 characters)</label>
       <br />
       <input id="title" type="text" name="postTitle" autoComplete="off"/>
       <br />
+      <label>Write a description for your post (max. 150 characters)</label>
+      <br />
+      <textarea id="description" name="postDescription" rows={3} autoComplete="off"></textarea>
+      <br />
       <label>Write your post</label>
       <br />
-      <textarea id="content" name="postContent" rows={8} autoComplete="off"></textarea>
+      <textarea id="content" name="postContent" rows={10} autoComplete="off"></textarea>
       <br />
-      <label>Enter the tags (separated by ;)</label>
+      <label>Enter your tags separated by semicolons (optional)</label>
       <br />
       <input />
+      <br />
+      <Link href="/">Cancel</Link>
+      <Btn1 text="Submit" />
     </CreateContainer>
     <Footer />
     </>
