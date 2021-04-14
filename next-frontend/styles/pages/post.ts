0e73286436edeb35ddbb2b8b7615abc58dc7d66b
André Nascimento <andrevsn1@gmail.com>
2021-04-14 09:18:36 -0300

    remove button style

--- next-frontend/styles/pages/post.ts
+++ next-frontend/styles/pages/post.ts
@@ -15,26 +15,4 @@ export const PostContainer = styled.div`
     color: ${props => props.theme.colors.date};
     font-size: 1rem;
   }
-
-  .return {
-    font-size: 1rem;
-    padding: 0.7rem 2rem;
-    border-radius: 10px;
-    border: 1px solid black;
-    background-color: ${props => props.theme.colors.bar};
-    margin-top: 4rem;
-  }
-
-  .return a {
-    color: black;
-    text-decoration: none;
-  }
-
-  .return:hover {
-    box-shadow: 3px 3px 6px black;
-  }
-
-  .return a:hover {
-    color: white;
-  }
 `
