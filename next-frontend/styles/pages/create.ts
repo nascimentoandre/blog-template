9e363ac5ca969601f5cccfbd6bb45a3d7de62397
André Nascimento <andrevsn1@gmail.com>
2021-04-14 09:18:00 -0300

    create page style

--- next-frontend/styles/pages/create.ts
+++ next-frontend/styles/pages/create.ts
@@ -2,4 +2,15 @@ import styled from "styled-components"
 
 export const CreateContainer = styled.form`
   margin: 2rem 5vw 5rem;
+
+  input, textarea {
+    width: 40vw;
+    margin-bottom: 1.5rem;
+  }
+
+  a {
+    margin-right: 2.2rem;
+    color: black;
+    font-size: 1.1rem;
+  }
 `
