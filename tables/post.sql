cdf4525179e00b0d924a4e425813c03d07d013d9
André Nascimento <andrevsn1@gmail.com>
2021-04-16 17:21:32 -0300

    include slug and sanitized_html columns

--- tables/post.sql
+++ tables/post.sql
@@ -1,8 +1,10 @@
 create table posts (
   id bigserial not null primary key,
   title varchar(70) not null,
+  slug varchar(100) not null unique,
   descript varchar(200),
   content text,
+  sanitized_html text,
   tags varchar(20)[],
   created_at timestamp default now()
 )
