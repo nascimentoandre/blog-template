e8ebdd40b30240415992fb06788e18c7a5eb991d
André Nascimento <andrevsn1@gmail.com>
2021-04-17 20:43:55 -0300

    include endpoint to delete posts

--- routes/posts.ts
+++ routes/posts.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPost, getAllPosts, getSinglePost } from "../controllers/postController";
+import { createPost, deletePost, getAllPosts, getSinglePost } from "../controllers/postController";
 
 const router = express.Router();
 
 router.get("/", getAllPosts);
 router.get("/:id", getSinglePost);
 router.post("/new", createPost);
+router.delete("/delete/:id", deletePost);
 
 export default router;
