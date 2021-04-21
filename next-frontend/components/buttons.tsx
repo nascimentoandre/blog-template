dd1db740384ec714a673522a9edcbface7345d54
André Nascimento <andrevsn1@gmail.com>
2021-04-21 10:08:09 -0300

    add functionality to del btn

--- next-frontend/components/buttons.tsx
+++ next-frontend/components/buttons.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link"
 import api from "../services/api"
 import { BtnStyle } from "../styles/button"
+import { useRouter } from "next/router"
 
 interface Props {
   text: string
   link?: boolean
   to?: string
+}
+
+interface DelBtnInterface {
+  id: number
 }
 
 export const EditBtn: React.FC = () => {
@@ -14,9 +19,22 @@ export const EditBtn: React.FC = () => {
   )
 }
 
-export const DeleteBtn: React.FC = () => {
+
+export const DeleteBtn: React.FC<DelBtnInterface> = ({ id }) => {
+  const router = useRouter()
+
+  const handleDelete: ((id:number) => void) = async (id) => {
+    try {
+      const del = await api.delete(`/posts/delete/${id}`)
+    } catch (err) {
+      console.error(err.message)
+    } finally {
+      router.push("/")
+    }
+  }
+
   return (
-    <BtnStyle style={{backgroundColor: "#EB2F23", marginRight: "3rem"}}>Delete</BtnStyle>
+    <BtnStyle style={{backgroundColor: "#EB2F23", marginRight: "3rem"}} onClick={() => handleDelete(id)}>Delete</BtnStyle>
   )
 }
 
